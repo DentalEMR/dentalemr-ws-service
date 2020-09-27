9984c555c1495b12b5bf4a1531aef41276557395
RM <russ@compass-point.net>
2020-09-27 10:04:56 -0700

    Updated install powershell script and dependencies

--- DemrService/InstallService.ps1
+++ DemrService/InstallService.ps1
@@ -4,8 +4,14 @@
 # more info on service users https://social.msdn.microsoft.com/forums/sqlserver/en-US/31d57870-1faa-4e14-8527-ce77b1ff40e4/local-service-local-system-or-network-service
 # more info on service user account: https://docs.microsoft.com/en-us/windows/win32/services/networkservice-account
 
-$appdir = "C:\Users\rm\source\repos\DemrService\publish-win"
-$appfullpath ="C:\Users\rm\source\repos\DemrService\publish-win\DemrService.exe"
+#INSTRUCTIONS: 
+# 1. run these commands, cancelling login dialog when it appears. Can copy/paste into powershell, or double click this file.
+# 2. confirm it is running in Services applet (lower left hand magnifying glass, type 'services', click 'services app')
+# 3. troubleshoot in wondows event viewer (lower left hand magnifying glass, type 'event', click 'event viewer', select 'windows logs', then 'application')
+
+
+$appdir = "C:\DentalEMR\DemrService"
+$appfullpath ="C:\DentalEMR\DemrService\DemrService.exe"
 $description = "Dental EMR Bridge"
 $displayname = "Dental EMR Bridge"
 
@@ -18,8 +24,9 @@ $acl.SetAccessRule($accessRule)
 $acl | Set-Acl $appfullpath
 
 New-Service -Name $servicename -BinaryPathName $appfullpath -Credential $user -Description $description -DisplayName $displayname -StartupType Automatic
+sc.exe start $servicename 
 
-#just cancel login
+
 
 # sc.exe delete $servicename
 # sc.exe stop $servicename
